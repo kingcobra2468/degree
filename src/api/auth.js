c6e197238ce50acdd10f7fec02f967acf5ad68f9
kingcobra2468 <eriktyryshkin@gmail.com>
2021-08-14 20:02:46 -0700

    fixed jwt secret and unauthorized exception message

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,13 +6,13 @@ module.exports = (server) => {
   server.post({ name: 'auth', path: '/api/auth' },
     (req, res, next) => {
       if (req.params.secret === undefined || req.params.secret !== process.env.AUTH_SECRET) {
-        return next(new errs.UnauthorizedError('invalid token'));
+        return next(new errs.UnauthorizedError('invalid secret'));
       }
 
       const timeNow = Math.floor(new Date() / 1000);
       const token = jwt.sign(
         { id: process.env.RPIST_ID, iat: timeNow },
-        process.env.AUTH_SECRET, { issuer: 'rpist' },
+        process.env.JWT_SECRET, { issuer: 'rpist' },
       );
       res.send({ jwt: token });
 
